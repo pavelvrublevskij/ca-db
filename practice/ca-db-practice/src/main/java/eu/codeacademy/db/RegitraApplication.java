4fa5b06e37484ea65f3589bd52550d3158464ae6
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2023-12-20 21:30:00 +0200

    [jdbc] save auto data into table, hardcoded version

--- practice/ca-db-practice/src/main/java/eu/codeacademy/db/RegitraApplication.java
+++ practice/ca-db-practice/src/main/java/eu/codeacademy/db/RegitraApplication.java
@@ -7,19 +7,31 @@ import java.sql.Statement;
 import java.util.List;
 
 import eu.codeacademy.db.model.Person;
+import eu.codeacademy.db.service.AutoService;
 import eu.codeacademy.db.service.PersonService;
 import eu.codeacademy.db.utils.JdbcUtil;
 
 public final class RegitraApplication {
 
 	private PersonService personService;
+	private AutoService autoService;
 
 	public void run() {
 		Connection connection = JdbcUtil.buildConnection();
 
 		personService = new PersonService(connection);
+		autoService = new AutoService(connection);
 		getSurnameExample();
 		getPersonAsObjectExample();
+		doAutoSaveExample();
+	}
+
+	private void doAutoSaveExample() {
+		try {
+			autoService.save(null);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	private void getPersonAsObjectExample() {
--- practice/ca-db-practice/src/main/java/eu/codeacademy/db/service/AutoService.java
+++ practice/ca-db-practice/src/main/java/eu/codeacademy/db/service/AutoService.java
@@ -1,6 +1,7 @@
 package eu.codeacademy.db.service;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -21,8 +22,11 @@ public class AutoService {
 	public void save(Auto auto) throws SQLException {
 		try {
 			connection.setAutoCommit(false);
-			Statement statement = connection.createStatement();
-			// TODO: padaryti saugojima
+			PreparedStatement statement = connection.prepareStatement(
+					"INSERT INTO auto (number) VALUES (?)");
+			statement.setString(1, "AAA111");
+			statement.execute();
+
 			connection.commit();
 		} catch (SQLException e) {
 			connection.rollback();
