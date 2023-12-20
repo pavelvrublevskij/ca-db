01db2ac0f406cebad1ba361985ea6242bd288098
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2023-12-20 20:00:39 +0200

    [jdbc] create connection constants

--- practice/ca-db-practice/src/main/java/eu/codeacademy/db/utils/JdbcUtil.java
+++ practice/ca-db-practice/src/main/java/eu/codeacademy/db/utils/JdbcUtil.java
@@ -6,14 +6,15 @@ import java.sql.SQLException;
 
 public final class JdbcUtil {
 
+	private static final String DB_URL = "jdbc:postgresql://localhost:5432/ca_app_db";
+	public static final String DB_USER = "app_user";
+	public static final String DB_PASSWORD = "app_user_password";
+
 	private JdbcUtil() {}
 
 	public static Connection buildConnection() {
 		try {
-			return DriverManager.getConnection(
-					"jdbc:postgresql://localhost:5432/ca_app_db",
-					"app_user",
-					"app_user_password");
+			return DriverManager.getConnection(DB_URL, DB_USER,	DB_PASSWORD);
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw new RuntimeException("ERROR -> Connection into database has a problem: " + e.getMessage());
