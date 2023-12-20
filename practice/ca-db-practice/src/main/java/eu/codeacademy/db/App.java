2f53cca4eee9ebfeb665b93039e8b9ae1b61e3aa
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2023-12-20 19:07:42 +0200

    [jdbc] use Statement and write first select query to get data using ResultSet

--- practice/ca-db-practice/src/main/java/eu/codeacademy/db/App.java
+++ practice/ca-db-practice/src/main/java/eu/codeacademy/db/App.java
@@ -2,7 +2,9 @@ package eu.codeacademy.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Hello world!
@@ -10,13 +12,21 @@ import java.sql.SQLException;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-
 		try {
 			Connection connection = DriverManager.getConnection(
 					"jdbc:postgresql://localhost:5432/ca_app_db",
 					"app_user",
 					"app_user_password");
+
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT * FROM person");
+
+			while (resultSet.next()) {
+				String surnameFromDB = resultSet.getString("surname");
+				System.out.println("Surname from db: " + surnameFromDB);
+			}
+
+			System.out.println("Database connected successfully!");
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw new RuntimeException("ERROR -> Connection into database has a problem: " + e.getMessage());
