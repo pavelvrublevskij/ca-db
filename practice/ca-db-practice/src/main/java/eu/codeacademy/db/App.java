5132197974417aa822e63c6ddc646eb028e842cd
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2023-12-20 18:36:06 +0200

    [jdbc] add Connection and DriverManager to connect into database using jdbc url + add driver from maven repo

--- practice/ca-db-practice/src/main/java/eu/codeacademy/db/App.java
+++ practice/ca-db-practice/src/main/java/eu/codeacademy/db/App.java
@@ -1,5 +1,9 @@
 package eu.codeacademy.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Hello world!
  */
@@ -7,5 +11,15 @@ public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+
+		try {
+			Connection connection = DriverManager.getConnection(
+					"jdbc:postgresql://localhost:5432/ca_app_db",
+					"app_user",
+					"app_user_password");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException("ERROR -> Connection into database has a problem: " + e.getMessage());
+		}
 	}
 }
