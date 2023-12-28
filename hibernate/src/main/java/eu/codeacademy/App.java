5dae35dceb65423ce8be5302a6f4757f6d7943fb
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2023-12-28 20:43:16 +0200

    hibernate - add hibernate (HQL) query example using createQuery method and return to the List

--- hibernate/src/main/java/eu/codeacademy/App.java
+++ hibernate/src/main/java/eu/codeacademy/App.java
@@ -9,18 +9,34 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.NativeQuery;
+import org.hibernate.query.Query;
 
 public class App {
 
 	public static void main(String[] args) {
+		System.out.println("Get data using native query (SQL)");
 		nativeQueryExample();
+
+		System.out.println("Get data using hibernate query (HQL)");
+		hqlQueryExample();
+	}
+
+	private static void hqlQueryExample() {
+		try (Session session = getSession()) {
+			Query<EmployeePojo> query = session.createQuery("FROM EmployeePojo", EmployeePojo.class);
+			List<EmployeePojo> employees = query.list();
+
+			employees.forEach(System.out::println);
+		}
 	}
 
 	private static void nativeQueryExample() {
-		NativeQuery<EmployeePojo> query = getSession().createNativeQuery("SELECT * FROM employee", EmployeePojo.class);
-		List<EmployeePojo> employees = query.list();
+		try (Session session = getSession()) {
+			NativeQuery<EmployeePojo> query = session.createNativeQuery("SELECT * FROM employee", EmployeePojo.class);
+			List<EmployeePojo> employees = query.list();
 
-		employees.forEach(System.out::println);
+			employees.forEach(System.out::println);
+		}
 	}
 
 	private static Session getSession() {
