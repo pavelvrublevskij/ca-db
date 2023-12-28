efa31e6a26073700ee8bf2e5b6d04039145df488
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2023-12-28 19:02:36 +0200

    hibernate - init hibernate with xml configuration file

--- hibernate/src/main/java/eu/codeacademy/App.java
+++ hibernate/src/main/java/eu/codeacademy/App.java
@@ -1,8 +1,13 @@
 package eu.codeacademy;
 
+import org.hibernate.cfg.Configuration;
+
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Configuration configuration = new Configuration();
+		configuration.configure().buildSessionFactory();
+
+		System.out.println("Hi from hibernate!!");
 	}
 }
